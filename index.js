e8d49a50e40b2c1d3fde32a74029abef35947096
Camila Kikuchi <camila_kikuchi@yahoo.com.br>
2021-03-25 11:50:42 -0300

    config heroku

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const routes = require('./server/routes/index')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const routerUsers = require("./server/routes/users");
+const routerProducts = require("./server/routes/products");
+const routerOrders = require("./server/routes/orders");
 
-app.use('/', routes);
+app.use("/users", routerUsers);
+app.use("/products", routerProducts);
+app.use("/orders", routerOrders);
 
-app.get('*', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
