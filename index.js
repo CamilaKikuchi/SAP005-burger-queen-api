754d65c06fa03386c42ea3a91f91a812d4e72ba3
Camila Kikuchi <camila_kikuchi@yahoo.com.br>
2021-04-02 12:48:37 -0300

    corigindo erro

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const routes = require('./server/routes/index')
 const app = express()
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', routes);
 
