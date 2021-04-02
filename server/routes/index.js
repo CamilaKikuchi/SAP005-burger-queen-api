d82b1ccc50fc4c1c5415a74f88e2064bd8508640
Camila Kikuchi <camila_kikuchi@yahoo.com.br>
2021-04-02 12:11:45 -0300

    correçoes e inserçao de models e migrations

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,13 @@
-const { Router } = require('express');
-const UsersRouter = require('./UsersRouter');
-const ProductsRouter = require('./ProductsRouter');
-const OrdersRouter = require('./OrdersRouter');
+const { Router } = require('express')
+const UsersRouter = require('./users')
+const ProductsRouter = require('./products')
+const OrdersRouter = require('./orders')
 
-const router = Router();
+const router = Router()
 
 // aqui vai todas as rotas
 router.use('/users', UsersRouter);
-router.use('/products', ProductsRouter);
-router.use('/orders', OrdersRouter);
+router.use('/products', ProductsRouter)
+router.use('/orders', OrdersRouter)
 
-module.exports = router;
+module.exports = router
