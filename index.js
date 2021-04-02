c70554459a162ee37db22c4d0f13f62f36a5d26d
Camila Kikuchi <camila_kikuchi@yahoo.com.br>
2021-04-02 13:36:30 -0300

    correçao de erros

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const routes = require('./server/routes/index')
 const app = express()
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', routes);
 
-/*app.get('*', (req, res) => {
-  res.send('Hello World!')
-})*/
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server is running on PORT ${port}`)
 })
+
